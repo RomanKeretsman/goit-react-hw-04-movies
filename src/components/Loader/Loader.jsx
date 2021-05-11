dd5e3aa7811087bbe738900db66d2c34581862b9
Roman Keretsman <rkeretsman@gmail.com>
2021-05-11 22:30:25 +0300

    add component Loader

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,17 @@
+import { createPortal } from 'react-dom';
+import Template from 'react-loader-spinner';
+
+import styles from './Loader.module.scss';
+
+const loaderRoot = document.querySelector('#loader-root');
+
+const Loader = () => {
+  return createPortal(
+    <div className={styles.loader}>
+      <Template type="Audio" color="#02be6e" height={100} width={100} />
+    </div>,
+    loaderRoot,
+  );
+};
+
+export default Loader;
